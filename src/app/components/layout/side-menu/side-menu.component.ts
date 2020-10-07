27da78eef18a46cb8767a24b7bf7b1cf3eb66696
pietrzakkuba <pietrzakkuba@yahoo.pl>
2020-10-07 09:02:14 +0200

    [#1][improved] auto-hide side menu after clicking link in smaller devices

--- src/app/components/layout/side-menu/side-menu.component.ts
+++ src/app/components/layout/side-menu/side-menu.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, AfterViewInit, Input} from '@angular/core';
 
 @Component({
   selector: 'app-side-menu',
   templateUrl: './side-menu.component.html',
   styleUrls: ['./side-menu.component.scss']
 })
-export class SideMenuComponent implements OnInit {
+export class SideMenuComponent implements OnInit, AfterViewInit {
 
+  @Input() sidenav;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit() {
+    const links = document.getElementsByClassName('link');
+    const that = this;
+    Array.from(links).forEach(link => {
+      link.addEventListener('click', () => {
+        if (that.sidenav.mode === 'over') {
+          that.sidenav.close();
+        }
+      });
+    });
   }
 
 }
