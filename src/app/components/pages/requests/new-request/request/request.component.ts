7ae0ece9d4d65a7cb357f56fcc7280fdb791c430
pietrzakkuba <pietrzakkuba@yahoo.pl>
2020-10-19 23:46:59 +0200

    [#3][added][WiP] dehardcoding form field appearance and made some fields readonly

--- src/app/components/pages/requests/new-request/request/request.component.ts
+++ src/app/components/pages/requests/new-request/request/request.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild, AfterViewInit, ChangeDetectorRef} from '@angular/core';
+import {MatFormFieldAppearance} from '@angular/material/form-field';
+import {MatInput} from '@angular/material/input';
 
 @Component({
   selector: 'app-request',
   templateUrl: './request.component.html',
   styleUrls: ['./request.component.scss']
 })
-export class RequestComponent implements OnInit {
+export class RequestComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  formFieldsStyle: MatFormFieldAppearance = 'standard';
+
+  @ViewChild('firstNameAndSurname', {read: MatInput}) firstNameAndSurname: MatInput;
+  @ViewChild('academicTitle', {read: MatInput}) academicTitle: MatInput;
+  @ViewChild('institute', {read: MatInput}) institute: MatInput;
+  @ViewChild('phoneNumber', {read: MatInput}) phoneNumber: MatInput;
+
+  constructor(
+    private changeDetectorRef: ChangeDetectorRef
+  ) {
+  }
 
   ngOnInit(): void {
+
+  }
+
+  ngAfterViewInit() {
+    this.disableAndSetAutocompletingFields();
+    this.changeDetectorRef.detectChanges();
+
+  }
+
+  disableAndSetAutocompletingFields() {
+    this.firstNameAndSurname.readonly = true;
+    this.academicTitle.readonly = true;
+    this.institute.readonly = true;
+    this.phoneNumber.readonly = true;
+    this.firstNameAndSurname.value = 'Jakub Pietrzak';
+    this.academicTitle.value = 'może niedługo inżynier';
+    this.institute.value = 'instytut pisania najlepszej inżynierki';
+    this.phoneNumber.value = '+48 123 456 789';
   }
 
 }
