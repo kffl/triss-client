2afdaa0c06cbd6a328c5662498af8d2f45f9d887
pietrzakkuba <pietrzakkuba@yahoo.pl>
2020-10-22 20:38:12 +0200

    [#3][added] collecting data from request form into js object on submit

--- src/app/components/pages/requests/new-request/request/request.component.ts
+++ src/app/components/pages/requests/new-request/request/request.component.ts
@@ -25,6 +25,10 @@ export class RequestComponent implements OnInit, AfterViewInit {
     {value: 0, name: 'Dowód osobisty'},
     {value: 1, name: 'Paszport'}
   ];
+  paymentTypes = [
+    {value: 0, name: 'Gotówka'},
+    {value: 1, name: 'Przelew'}
+  ];
 
   // basic-info
   @ViewChild('firstNameAndSurname', {read: MatInput}) firstNameAndSurname: MatInput;
@@ -60,8 +64,30 @@ export class RequestComponent implements OnInit, AfterViewInit {
   @ViewChild('selfInsuredCheckbox', {read: MatCheckbox}) selfInsuredCheckbox: MatCheckbox;
 
   // advance-payment-request
+  @ViewChild('requestPaymentDestination', {read: MatInput}) requestPaymentDestination: MatInput;
+  @ViewChild('requestPaymentDate') requestPaymentDate: MatDateRangeInput<Date>;
+  @ViewChild('requestPaymentDays', {read: MatInput}) requestPaymentDays: MatInput;
+  @ViewChild('requestPaymentDaysAmount', {read: MatInput}) requestPaymentDaysAmount: MatInput;
+  @ViewChild('requestPaymentAccommodation', {read: MatInput}) requestPaymentAccommodation: MatInput;
+  @ViewChild('requestPaymentAccommodationLimit', {read: MatInput}) requestPaymentAccommodationLimit: MatInput;
+  @ViewChild('requestPaymentTravelDiet', {read: MatInput}) requestPaymentTravelDiet: MatInput;
+  @ViewChild('requestPaymentLocalTransportCosts', {read: MatInput}) requestPaymentLocalTransportCosts: MatInput;
+  @ViewChild('requestPaymentOtherExpensesDescription', {read: MatInput}) requestPaymentOtherExpensesDescription: MatInput;
+  @ViewChild('requestPaymentOtherExpensesValue', {read: MatInput}) requestPaymentOtherExpensesValue: MatInput;
+  @ViewChild('requestPaymentSummarizedCosts', {read: MatInput}) requestPaymentSummarizedCosts: MatInput;
 
+  // advance-payments
+  @ViewChild('conferenceFeeValue', {read: MatInput}) conferenceFeeValue: MatInput;
+  @ViewChild('conferenceFeePaymentTypeSelect') conferenceFeePaymentTypeSelect: MatSelect;
+  @ViewChild('depositValue', {read: MatInput}) depositValue: MatInput;
+  @ViewChild('depositPaymentTypeSelect') depositPaymentTypeSelect: MatSelect;
 
+  // identity-document
+  @ViewChild('identityDocumentTypeSelect') identityDocumentTypeSelect: MatSelect;
+  @ViewChild('identityDocumentSerialNumber', {read: MatInput}) identityDocumentSerialNumber: MatInput;
+
+  // comments
+  @ViewChild('comments', {read: MatInput}) comments: MatInput;
 
   constructor(
     private changeDetectorRef: ChangeDetectorRef
@@ -86,6 +112,10 @@ export class RequestComponent implements OnInit, AfterViewInit {
     this.academicTitle.value = 'może niedługo inżynier';
     this.institute.value = 'instytut pisania najlepszej inżynierki';
     this.phoneNumber.value = '+48 123 456 789';
+    this.allocationAccount.disabled = true;
+    this.MPK.disabled = true;
+    this.financialSource.disabled = true;
+    this.project.disabled = true;
   }
 
   submitForm(event: Event) {
@@ -112,33 +142,61 @@ export class RequestComponent implements OnInit, AfterViewInit {
         project: this.project.value
       },
       transport: {
-        vehicleSelect: this.vehicleSelect.value ? this.vehicleSelect.value : '',
+        vehicleSelect: this.formatSelect(this.vehicleSelect.value),
         route: this.route.value,
-        departureMinute: this.departureMinuteSelect.value ? this.departureMinuteSelect.value : '',
-        departureHour: this.departureHourSelect.value ? this.departureHourSelect.value : '',
+        departureMinute: this.formatSelect(this.departureMinuteSelect.value),
+        departureHour: this.formatSelect(this.departureMinuteSelect.value),
         departureDay: this.formatDate(this.departureDate.value),
         carrier: this.carrier.value
       },
       insurance: {
         firstNameAndSurnameInsurance: this.firstNameAndSurnameInsurance.value,
         birthDate: this.formatDate(this.birthDate.value),
-        departureCountry: this.departureCountry,
+        departureCountry: this.departureCountry.value,
         abroadStartDateInsurance: this.formatDate(this.abroadDateInsurance.value.start),
         abroadEndDateInsurance: this.formatDate(this.abroadDateInsurance.value.end),
         selfInsuredCheckbox: this.selfInsuredCheckbox.checked
       },
       advancePaymentRequest: {
-        // destination: this.destination.value,
+        requestPaymentDestination: this.requestPaymentDestination.value,
+        requestPaymentStartDate: this.formatDate(this.requestPaymentDate.value.start),
+        requestPaymentEndDate: this.formatDate(this.requestPaymentDate.value.end),
+        requestPaymentDays: this.requestPaymentDays.value,
+        requestPaymentDaysAmount: this.requestPaymentDaysAmount.value,
+        requestPaymentAccommodation: this.requestPaymentAccommodation.value,
+        requestPaymentAccommodationLimit: this.requestPaymentAccommodationLimit.value,
+        requestPaymentTravelDiet: this.requestPaymentTravelDiet.value,
+        requestPaymentLocalTransportCosts: this.requestPaymentLocalTransportCosts.value,
+        requestPaymentOtherExpensesDescription: this.requestPaymentOtherExpensesDescription.value,
+        requestPaymentOtherExpensesValue: this.requestPaymentOtherExpensesValue.value,
+        requestPaymentSummarizedCosts: this.requestPaymentSummarizedCosts.value
       },
-      advancePayments: {},
-      identityDocument: {},
-      comments: {}
+      advancePayments: {
+        conferenceFeeValue: this.conferenceFeeValue.value,
+        conferenceFeePaymentTypeSelect: this.formatSelect(this.conferenceFeePaymentTypeSelect.value),
+        depositValue: this.depositValue.value,
+        depositPaymentTypeSelect: this.formatSelect(this.depositPaymentTypeSelect.value),
+      },
+      identityDocument: {
+        identityDocumentTypeSelect: this.formatSelect(this.identityDocumentTypeSelect.value),
+        identityDocumentSerialNumber: this.identityDocumentSerialNumber.value
+      },
+      comments: {
+        comments: this.comments.value
+      }
     };
   }
 
   formatDate(date: Date): string {
     return date ?
       `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}` : '';
+  }
+
+  formatSelect(value: any): any {
+    if (value == null) {
+      return '';
+    }
+    return value;
   }
 
 }
