bfcf3a27d30437b27c871d5ef4bdc4f09438821c
pietrzakkuba <pietrzakkuba@yahoo.pl>
2021-01-21 01:30:46 +0100

    deleted unused import

--- src/app/components/shared/security/SafeHttpClient.ts
+++ src/app/components/shared/security/SafeHttpClient.ts
@@ -1,7 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs';
-import {SecurityService} from './SecurityService';
 import {LocalStorageService} from './LocalStorageService';
 
 
